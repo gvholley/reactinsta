f63e300510889cd4b5c729014b2bdc4c853e0dbc
Grant Holley <gvholley@gmail.com>
2020-12-04 19:41:23 +0900

    Edited header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_header">
+        <img
+          className="app_headerImage"
+          src="https://i.pinimg.com/originals/fc/26/50/fc26502254db62ce6b29debec1a56e80.png"
+          alt="logo"
+        />
+      </div>
+      <h1>Hi</h1>
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Post() {
+  return (
+    <div>
+  {/* header -> avatar +username */}
+
+  {/* image */}
+
+{/* username + caption */}
+    </div>
+    )
+}
+
+export default Post
