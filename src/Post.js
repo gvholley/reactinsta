bf8abe42d8f3c928b77e7e938e6fab63e33a55ad
Grant Holley <gvholley@gmail.com>
2020-12-05 21:13:45 +0900

    Added avatar

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
     <div className="post">
+    <Avatar
+      className="post_avatar"
+      alt='Kaori'
+      src="/static/images/avatar/1.jpg"
+      />
     <h3>Username</h3>
     <img className="post_image" src="https://pbs.twimg.com/media/EnrPcbGVoAEZe2P?format=jpg&name=large" alt="" />
     <h4 className="post_text"><strong>Grant87</strong>: So beautiful!</h4>
