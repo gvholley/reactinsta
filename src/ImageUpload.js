7b5eaa786f5cfd728e48a31da9499f22dec7bb3b
Grant Holley <gvholley@gmail.com>
2020-12-08 18:19:04 +0900

    Added styling to image upload

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -48,7 +48,7 @@ function ImageUpload({username}) {
 
   return (
     <div className="imageupload">
-      <progress value={progress} max="100" />
+      <progress className="imageupload_progress" value={progress} max="100" />
       <input type="text" placeholder="Enter a caption" onChange={event => setCaption(event.target.value)} value={caption} />
       <input type="file" onChange={handleChange} />
       <Button onClick={handleUpload}>
